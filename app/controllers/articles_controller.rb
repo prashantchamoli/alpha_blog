01770da36a24b77dedc72c58c629bb2cc294b59b
prashant <pchamoli@starheadtech.com>
2019-02-07 21:24:20 +0530

    created edit, new, and show actions on index as well as inner pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    @article = Article.all
+  end
   def new
     @article = Article.new
   end
@@ -12,7 +15,19 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @article = Article.find(params[:id])
 
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article Successfully Edited"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
   def show
     @article = Article.find(params[:id])
   end
